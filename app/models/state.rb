861195514ff2f3cb9d8daf2c2248a57c72ab3705
Igor Belo <igorcoura@gmail.com>
2015-10-20 20:02:22 -0200

    adiciona modelo estado

---
+++ app/models/state.rb
@@ -0,0 +1,33 @@
+class State
+  UFS = %w(AC AL AP AM BA CE DF ES GO MA MT MS MG PA PB PR PE PI RJ RN RS RO RR SC SP SE TO)
+
+  ALL = {
+    AC: 'Acre',
+    AL: 'Alagoas',
+    AP: 'Amapá',
+    AM: 'Amazonas',
+    BA: "Bahia",
+    CE: 'Ceará',
+    DF: 'Distrito Federal',
+    ES: "Espírito Santo",
+    GO: 'Goias',
+    MA: 'Maranhão',
+    MT: 'Mato Grosso',
+    MS: 'Mato Grosso do Sul',
+    MG: "Minas Gerais",
+    PA: 'Pará',
+    PB: 'Paraíba',
+    PR: 'Paraná',
+    PE: 'Pernambuco',
+    PI: 'Piauí',
+    RJ: 'Rio de Janeiro',
+    RN: 'Rio Grande do Norte',
+    RS: 'Rio Grande do Sul',
+    RO: 'Rondônia',
+    RR: 'Roraima',
+    SC: 'Santa Catarina',
+    SP: 'São Paulo',
+    SE: 'Sergipe',
+    TO: 'Tocantins'
+  }
+end

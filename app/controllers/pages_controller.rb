d02134c72b86c0bf119915dc26312c9896382bca
Igor Belo <igorcoura@gmail.com>
2015-10-21 00:09:47 -0200

    adiciona relatorio de peladas ocorrendo no momento

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
+require 'date'
+
 class PagesController < ApplicationController
   def home
-
+    @matches_running = Match.includes(place: :arenas).where("start <= :now AND end >= :now", now: DateTime.now).where(visibility: "public")
   end
 end
